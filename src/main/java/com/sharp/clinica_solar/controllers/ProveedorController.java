349543e285b06c9a9ec6fe599e1a4b0f2c3a3d49
Sebastian Salvador Blas Vega <i202403560@cibertec.edu.pe>
2025-06-21 23:42:44 -0500

    Creacion de Clases
    proveedorcontroller,proveedorrepository,proveedorservice oño

---
+++ src/main/java/com/sharp/clinica_solar/controllers/ProveedorController.java
@@ -0,0 +1,5 @@
+package com.sharp.clinica_solar.controllers;
+
+public class ProveedorController {
+
+}
---
+++ src/main/java/com/sharp/clinica_solar/repositories/ProveedorRepository.java
@@ -0,0 +1,5 @@
+package com.sharp.clinica_solar.repositories;
+
+public interface ProveedorRepository {
+
+}
