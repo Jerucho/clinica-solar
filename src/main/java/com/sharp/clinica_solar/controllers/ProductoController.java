f35d64d611cf283ef35cd5f1da9daa2e941d5093
Sebastian Salvador Blas Vega <i202403560@cibertec.edu.pe>
2025-06-28 00:44:36 -0500

    Arreglo de bugs
    funcionamiento de vista agg productos,y restController

--- src/main/java/com/sharp/clinica_solar/controllers/ProductoController.java
+++ src/main/java/com/sharp/clinica_solar/controllers/ProductoController.java
@@ -10,10 +10,13 @@ import com.sharp.clinica_solar.models.Producto;
 import com.sharp.clinica_solar.repositories.ProductoRepository;
 import com.sharp.clinica_solar.services.ElementoService;
 
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/elementos")
 public class ProductoController {
 
@@ -41,12 +44,12 @@ public class ProductoController {
             return "redirect:/jefeareas/productos";
         }
     }
-
+    
     @PostMapping("/guardar")
-    public String guardarProducto(@ModelAttribute Elemento elemento) {
-        elementoService.guardarElemento(elemento);
-        return "redirect:/jefeareas/productos";
-    }
+	public void guardarProducto(@ModelAttribute Elemento elemento, HttpServletResponse response) throws IOException {
+		elementoService.guardarElemento(elemento);
+		response.sendRedirect("/jefeareas/productos");
+	}
 
     @GetMapping("/eliminar/{id}")
     public String eliminarProducto(@PathVariable Long id ) {
